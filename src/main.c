44deaf96266032f90d1c6ddc5fd84d677871ccb9
anima-libera <anim.libera@gmail.com>
2023-02-18 01:14:11 +0100

    Added functions and made a separate call stack

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct buf_t
 {
@@ -48,22 +49,22 @@ void buf_overwrite(buf_t* buf, uint64_t offset, uint8_t const* new_stuff, uint64
 	memcpy(&buf->arr[offset], new_stuff, size);
 }
 
-struct data_addr_ofs_t
+struct addr_ofs_t
 {
 	uint64_t offset;
 	uint64_t size;
-	uint64_t value;
+	uint64_t value; // base value for data addr, func index for func addr
 };
-typedef struct data_addr_ofs_t data_addr_ofs_t;
+typedef struct addr_ofs_t addr_ofs_t;
 
-struct da_data_addr_ofs_t
+struct da_addr_ofs_t
 {
 	uint64_t len, cap;
-	data_addr_ofs_t* arr;
+	addr_ofs_t* arr;
 };
-typedef struct da_data_addr_ofs_t da_data_addr_ofs_t;
+typedef struct da_addr_ofs_t da_addr_ofs_t;
 
-void da_da_data_addr_ofs_append(da_data_addr_ofs_t* da, data_addr_ofs_t data_addr_ofs)
+void da_addr_ofs_append(da_addr_ofs_t* da, addr_ofs_t data_addr_ofs)
 {
 	RESIZE_DA(da->cap, da->arr, da->len + 1);
 	da->arr[da->len] = data_addr_ofs;
@@ -74,9 +75,14 @@ struct instr_t
 {
 	enum instr_type_t
 	{
+		INSTR_INIT_PROG,
+		INSTR_HALT_PROG,
+		INSTR_PUSH_FUNC,
+		INSTR_CALL_POP,
+		INSTR_INIT_FUNC,
+		INSTR_RETURN_FUNC,
 		INSTR_PUSH_IMM,
 		INSTR_PRINT_CHAR,
-		INSTR_HALT,
 	}
 	type;
 	uint64_t value;
@@ -97,20 +103,65 @@ void da_instr_append(da_instr_t* da, instr_t instr)
 	da->len++;
 }
 
-int main(int argc, char const* const* argv)
+struct func_t
 {
-	assert(argc == 2);
-	char const* src = argv[1];
-	uint64_t src_len = strlen(src);
-	da_instr_t code = {0};
+	da_instr_t code;
+};
+typedef struct func_t func_t;
 
-	// Parsing
+struct da_func_t
+{
+	uint64_t len, cap;
+	func_t* arr;
+};
+typedef struct da_func_t da_func_t;
+
+void da_func_append(da_func_t* da, func_t func)
+{
+	RESIZE_DA(da->cap, da->arr, da->len + 1);
+	da->arr[da->len] = func;
+	da->len++;
+}
+
+uint64_t parse_func(da_func_t* da, uint64_t self_index, char const* src, bool is_entry_point)
+{
+	assert(da->len == self_index);
+	da_func_append(da, (func_t){0});
+	func_t* func = &da->arr[self_index];
+	if (is_entry_point)
+	{
+		da_instr_append(&func->code,
+			(instr_t){.type = INSTR_INIT_PROG});
+	}
+	else
+	{
+		da_instr_append(&func->code,
+			(instr_t){.type = INSTR_INIT_FUNC});
+	}
 	uint64_t i = 0;
-	while (i < src_len)
+	while ((src[i] != '\0' && is_entry_point) || (src[i] != ']' && !is_entry_point))
 	{
 		if (src[i] == ' ')
 		{
 			i++;
+		}
+		else if (src[i] == '[')
+		{
+			// `[...]` pushes the address of a function that does `...`
+			// one can use `c` to call it
+			i++;
+			uint64_t sub_func_index = da->len;
+			i += parse_func(da, sub_func_index, &src[i], false);
+			assert(src[i] == ']');
+			i++;
+			da_instr_append(&func->code,
+				(instr_t){.type = INSTR_PUSH_FUNC, .value = sub_func_index});
+		}
+		else if (src[i] == 'c')
+		{
+			i++;
+			da_instr_append(&func->code,
+				(instr_t){.type = INSTR_CALL_POP});
 		}
 		else if ('0' <= src[i] && src[i] <= '9')
 		{
@@ -120,24 +171,53 @@ int main(int argc, char const* const* argv)
 				value = value * 10 + src[i] - '0';
 				i++;
 			}
-			da_instr_append(&code, (instr_t){.type = INSTR_PUSH_IMM, .value = value});
+			da_instr_append(&func->code,
+				(instr_t){.type = INSTR_PUSH_IMM, .value = value});
 		}
 		else if (src[i] == 'p')
 		{
 			i++;
-			da_instr_append(&code, (instr_t){.type = INSTR_PRINT_CHAR});
+			da_instr_append(&func->code,
+				(instr_t){.type = INSTR_PRINT_CHAR});
 		}
 		else if (src[i] == 'h')
 		{
 			i++;
-			da_instr_append(&code, (instr_t){.type = INSTR_HALT});
+			da_instr_append(&func->code,
+				(instr_t){.type = INSTR_HALT_PROG});
+		}
+		else if (src[i] == 'r')
+		{
+			i++;
+			da_instr_append(&func->code,
+				(instr_t){.type = INSTR_RETURN_FUNC});
 		}
 		else
 		{
-			assert(0);
+			assert(false);
 		}
 	}
-	da_instr_append(&code, (instr_t){.type = INSTR_HALT});
+	if (is_entry_point)
+	{
+		da_instr_append(&func->code,
+			(instr_t){.type = INSTR_HALT_PROG});
+	}
+	else
+	{
+		da_instr_append(&func->code,
+			(instr_t){.type = INSTR_RETURN_FUNC});
+	}
+	return i;
+}
+
+int main(int argc, char const* const* argv)
+{
+	assert(argc == 2);
+	char const* src = argv[1];
+
+	// Parsing
+	da_func_t func_da = {0};
+	parse_func(&func_da, 0, src, true);
 
 	// Buffer
 	buf_t bin = {0};
@@ -235,6 +315,19 @@ int main(int argc, char const* const* argv)
 		OVWLE64(data_segment_address_ofs_2, (offset_) + data_segment_address); \
 		OVWLE64(data_segment_size_ofs_1, (size_)); \
 		OVWLE64(data_segment_size_ofs_2, (size_))
+
+	// Spine call stack
+	APPLE32(1); // Loadable segment
+	APPLE32((1<<0/*Readable*/) | (1<<1/*Writable*/)); // Flags
+	APPLE64(0); // Offset of segment in binary
+	uint64_t call_stack_segment_address = 0x800000;
+	APPLE64(call_stack_segment_address); // Address of segment in virtual memory
+	APPLE64(call_stack_segment_address); // Address of segment in physical memory (wtf)
+	APPLE64(0); // Segment size in binary
+	uint64_t call_stack_segment_size = 0x4000;
+	APPLE64(call_stack_segment_size); // Segment size in memory
+	APPLE64(0); // Alignment
+	program_header_table_length++;
 	
 	OVWLE16(program_header_table_length_ofs, program_header_table_length);
 
@@ -246,32 +339,62 @@ int main(int argc, char const* const* argv)
 	enum reg_t { RAX = 0, RBX = 3, RCX = 1, RDX = 2, RSP = 4, RBP = 5, RSI = 6, RDI = 7 };
 	#define GET_BIT(v_, bit_index_)	\
 		((((uint64_t)(v_)) >> (bit_index_)) & 1)
+	// For `mod_` see https://wiki.osdev.org/X86-64_Instruction_Encoding#32.2F64-bit_addressing
 	#define MODRM(mod_, reg_, rm_) \
 		BITS(GET_BIT(mod_, 1), GET_BIT(mod_, 0), \
 			GET_BIT(reg_, 2), GET_BIT(reg_, 1), GET_BIT(reg_, 0), \
 			GET_BIT(rm_, 2), GET_BIT(rm_, 1), GET_BIT(rm_, 0))
 	#define MOD11 \
 		((1<<1)|(1<<0))
+	#define MOD01 \
+		((0<<1)|(1<<0))
+	#define MOD00 \
+		((0<<1)|(0<<0))
+
 	#define SYSCALL() \
 		APPBYTES(0x0f, 0x05)
 
-	struct da_data_addr_ofs_t data_address_offsets = {0};
+	struct da_addr_ofs_t data_address_offsets = {0};
 	#define APPDATAADDR32(v_) \
-		da_da_data_addr_ofs_append(&data_address_offsets, \
-			(data_addr_ofs_t){.offset = APPLE32(v_), .size = 32, .value = (v_)})
+		da_addr_ofs_append(&data_address_offsets, \
+			(addr_ofs_t){.offset = APPLE32(v_), .size = 32, .value = (v_)})
+
+	struct da_addr_ofs_t func_address_offsets = {0};
+	#define APPFUNCADDR32(func_index_) \
+		da_addr_ofs_append(&func_address_offsets, \
+			(addr_ofs_t){.offset = APPLE32(func_index_), .size = 32, .value = (func_index_)})
 	
 	#define MOV_IMM32_TO_R64(imm32_, reg64_) \
 		APPBYTES(REX(1,0,0,0), 0xc7, MODRM(MOD11, 0, (reg64_))); APPLE32(imm32_)
 	#define MOV_DATAADDR32_TO_R64(imm32_, reg64_) \
 		APPBYTES(REX(1,0,0,0), 0xc7, MODRM(MOD11, 0, (reg64_))); APPDATAADDR32(imm32_)
+	#define MOV_FUNCADDR32_TO_R64(func_index_, reg64_) \
+		APPBYTES(REX(1,0,0,0), 0xc7, MODRM(MOD11, 0, (reg64_))); APPFUNCADDR32(func_index_)
 
 	#define MOV_R64_TO_R64(reg64_src_, reg64_dst_) \
 		APPBYTES(REX(1,0,0,0), 0x89, MODRM(MOD11, (reg64_src_), (reg64_dst_)))
 
+	#define MOV_R64_TO_MEMPOINTEDBY_R64(reg64_src_, reg64_pointingto_dst_) \
+		APPBYTES(REX(1,0,0,0), 0x89, MODRM(MOD01, (reg64_src_), (reg64_pointingto_dst_)), 0)
+	#define MOV_MEMPOINTEDBY_R64_TO_R64(reg64_pointingto_src_, reg64_dst_) \
+		APPBYTES(REX(1,0,0,0), 0x8b, MODRM(MOD01, (reg64_dst_), (reg64_pointingto_src_)), 0)
+
 	#define PUSH_IMM32(imm32_) \
 		APPBYTES(0x68); APPLE32(imm32_)
-	#define POP32_TO_R64(reg64_) \
+	#define POP64_TO_R64(reg64_) \
 		APPBYTES(0x58 + (reg64_))
+	#define PUSH_R64(reg64_) \
+		APPBYTES(0x50 + reg64_)
+		
+	#define CALL_R64(reg64_) \
+		APPBYTES(0xff, MODRM(MOD11, 2, (reg64_)))
+	#define RET() \
+		APPBYTES(0xc3)
+
+	#define ADD_IMM32_TO_R64(imm32_, reg64_) \
+		APPBYTES(REX(1,0,0,0), 0x81, MODRM(MOD11, 0, reg64_)); APPLE32(imm32_)
+	#define SUB_IMM32_TO_R64(imm32_, reg64_) \
+		APPBYTES(REX(1,0,0,0), 0x81, MODRM(MOD11, 5, reg64_)); APPLE32(imm32_)
 
 	// Program code
 	uint64_t code_offset = bin.len;
@@ -287,39 +410,101 @@ int main(int argc, char const* const* argv)
 	SYSCALL();
 	#endif
 
-	for (uint64_t i = 0; i < code.len; i++)
+	uint64_t func_offset[func_da.len];
+
+	for (uint64_t func_i = 0; func_i < func_da.len; func_i++)
 	{
-		instr_t instr = code.arr[i];
-		switch (instr.type)
+		func_offset[func_i] = bin.len;
+		for (uint64_t instr_i = 0; instr_i < func_da.arr[func_i].code.len; instr_i++)
 		{
-			case INSTR_PUSH_IMM:
-				PUSH_IMM32(instr.value);
-			break;
-			case INSTR_PRINT_CHAR:
-				MOV_IMM32_TO_R64(1, RAX); // `write` syscall number
-				MOV_IMM32_TO_R64(1, RDI); // `stdout` file descriptor
-				MOV_R64_TO_R64(RSP, RSI); // ptr to string is stack ptr
-				MOV_IMM32_TO_R64(1, RDX); // print 1 char
-				SYSCALL();
-				POP32_TO_R64(RAX); // pop to discard
-			break;
-			case INSTR_HALT:
-				MOV_IMM32_TO_R64(60, RAX); // `exit` syscall number
-				MOV_IMM32_TO_R64(0, RDI); // exit code value
-				SYSCALL();
-			break;
-			default:
-				assert(0);
-			break;
+			instr_t instr = func_da.arr[func_i].code.arr[instr_i];
+			switch (instr.type)
+			{
+				case INSTR_INIT_PROG:
+					// %rbp is used as the spine call stack pointer
+					MOV_IMM32_TO_R64(call_stack_segment_address, RBP);
+				break;
+				case INSTR_HALT_PROG:
+					MOV_IMM32_TO_R64(60, RAX); // `exit` syscall number
+					MOV_IMM32_TO_R64(0, RDI); // exit code value
+					SYSCALL();
+				break;
+				case INSTR_PUSH_FUNC:
+					MOV_FUNCADDR32_TO_R64(instr.value, RAX);
+					PUSH_R64(RAX);
+				break;
+				case INSTR_CALL_POP:
+					POP64_TO_R64(RAX);
+					CALL_R64(RAX);
+				break;
+				case INSTR_INIT_FUNC:
+					// The `call` to here pushed the return address on the data stack
+					// so we move it to the spine call stack
+					POP64_TO_R64(RAX);
+					MOV_R64_TO_MEMPOINTEDBY_R64(RAX, RBP);
+					ADD_IMM32_TO_R64(8, RBP);
+				break;
+				case INSTR_RETURN_FUNC:
+					// The return address was put on the spine call stack
+					// so we move it back to the data stack for `ret` to work
+					SUB_IMM32_TO_R64(8, RBP);
+					MOV_MEMPOINTEDBY_R64_TO_R64(RBP, RAX);
+					PUSH_R64(RAX);
+					RET();
+				break;
+				case INSTR_PUSH_IMM:
+					PUSH_IMM32(instr.value);
+				break;
+				case INSTR_PRINT_CHAR:
+					MOV_IMM32_TO_R64(1, RAX); // `write` syscall number
+					MOV_IMM32_TO_R64(1, RDI); // `stdout` file descriptor
+					MOV_R64_TO_R64(RSP, RSI); // ptr to string is stack ptr
+					MOV_IMM32_TO_R64(1, RDX); // print 1 char
+					SYSCALL();
+					POP64_TO_R64(RAX); // pop to discard
+				break;
+				default:
+					assert(false);
+				break;
+			}
+		}
+	}
+
+	// Fill addresses of functions
+	for (uint64_t i = 0; i < func_address_offsets.len; i++)
+	{
+		addr_ofs_t func_address_offset = func_address_offsets.arr[i];
+		if (func_address_offset.size == 32)
+		{
+			OVWLE32(func_address_offset.offset,
+				code_segment_address + func_offset[func_address_offset.value]);
+		}
+		else if (func_address_offset.size == 64)
+		{
+			OVWLE64(func_address_offset.offset,
+				code_segment_address + func_offset[func_address_offset.value]);
+		}
+		else
+		{
+			assert(false);
 		}
 	}
 
 	// End of code
 	COMPLETE_CODE_SEGMENT_INFO(code_offset, bin.len - code_offset);
 
+	#if 0
+		printf("base address: 0x%x\n", code_offset);
+		for (uint64_t i = code_offset; i < bin.len; i++)
+		{
+			printf("%02x ", bin.arr[i]);
+		}
+		printf("\n");
+	#endif
+
 	// Data
 	#if 0
-	buf_append_zeros(&bin, bin.len % (8 * 4096)); // Page align (is this necessary?)
+		buf_append_zeros(&bin, bin.len % (8 * 4096)); // Page align (is this necessary?)
 	#endif
 	uint64_t data_offset = bin.len;
 	COMPLETE_DATA_SEGMENT_INFO(data_offset, data.len);
@@ -328,7 +513,7 @@ int main(int argc, char const* const* argv)
 	// Correct addresses that are supposed to point to stuff in the data segment
 	for (uint64_t i = 0; i < data_address_offsets.len; i++)
 	{
-		data_addr_ofs_t data_address_offset = data_address_offsets.arr[i];
+		addr_ofs_t data_address_offset = data_address_offsets.arr[i];
 		if (data_address_offset.size == 32)
 		{
 			OVWLE32(data_address_offset.offset, data_address_offset.value + data_offset);
@@ -339,7 +524,7 @@ int main(int argc, char const* const* argv)
 		}
 		else
 		{
-			assert(0);
+			assert(false);
 		}
 	}
 
